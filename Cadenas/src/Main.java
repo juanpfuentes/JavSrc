42efd4081f561a28dfa5c29ebb6126b605e2c9ac
Juan Pablo Fuentes <incidencias@gmail.com>
2023-01-23 12:56:08 +0100

    Cadenas

---
+++ Cadenas/src/Main.java
@@ -0,0 +1,32 @@
+
+import com.trifulcas.utilStrings.UtilsString;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(invertir2("Hello world!"));
+        System.out.println(invertir("Hello world!"));
+        System.out.println(UtilsString.invertir("Hello world!"));
+        System.out.println(UtilsString.contarChar("Hola que tal",'a'));
+        System.out.println(UtilsString.sinEspacios("   Hola      que    tal "));
+        System.out.println(UtilsString.palindromo(("qwqw")));
+        System.out.println(UtilsString.palindromo(("ana")));
+        System.out.println(UtilsString.palindromo(("Isaac no ronca así")));
+        System.out.println(UtilsString.quitarAcentos(("Áéíüñòâ")));
+        System.out.println(UtilsString.vocales("Alegría"));
+    }
+
+
+    public static String invertir(String cadena) {
+        String res = "";
+        for (int i = cadena.length() - 1; i >= 0; i--) {
+            res += cadena.charAt(i);
+        }
+        return res;
+    }
+
+    public static String invertir2(String cadena) {
+        StringBuilder res = new StringBuilder(cadena);
+        return res.reverse().toString();
+    }
+}
--- Cadenas/src/com/trifulcas/utilStrings/UtilsString.java
+++ Cadenas/src/com/trifulcas/utilStrings/UtilsString.java
@@ -1,2 +1,52 @@
-package com.trifulcas.utilStrings;public class UtilsString {
+package com.trifulcas.utilStrings;
+
+import java.text.Normalizer;
+
+public class UtilsString {
+    public static String invertir(String cadena) {
+        StringBuilder res = new StringBuilder(cadena);
+        return res.reverse().toString();
+    }
+
+    public static int contarChar(String cadena,char car){
+        int res=0;
+        for(int i=0;i<cadena.length();i++){
+            if (cadena.charAt(i)==car){
+                res++;
+            }
+        }
+        return res;
+    }
+    public static long contarCharStream(String cadena,char car){
+        long res=cadena.chars().filter(x->x==car).count();
+        return res;
+    }
+
+    public static String sinEspacios(String cadena){
+        cadena=cadena.trim();
+        while(cadena.indexOf("  ")>=0){
+            cadena=cadena.replace("  "," ");
+        }
+        return cadena;
+    }
+    public static boolean palindromo(String cadena){
+        cadena=quitarAcentos(cadena).replaceAll("[^A-Za-z]+", "").toUpperCase();
+        return cadena.equals(invertir(cadena));
+    }
+
+    public static String quitarAcentos(String text) {
+        return text == null ? null :
+                Normalizer.normalize(text, Normalizer.Form.NFD)
+                        .replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+    }
+
+    public static int vocales(String cadena){
+        cadena=quitarAcentos(cadena).toLowerCase();
+        String voc="aeiou";
+        int res=0;
+        for(char c:voc.toCharArray()){
+            res+=contarChar(cadena,c);
+        }
+        return res;
+    }
 }
